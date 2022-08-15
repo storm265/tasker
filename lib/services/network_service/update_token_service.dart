921f855dc30609b0513008517dc98f0883b50570
peter3374 <peter4533@mail.ru>
2022-08-16 00:30:35 +0300

    refactor_refresh_token

--- lib/services/network_service/update_token_service.dart
+++ lib/services/network_service/update_token_service.dart
@@ -1,6 +1,4 @@
 import 'dart:developer';
-import 'package:flutter/material.dart';
-import 'package:todo2/database/data_source/auth_data_source.dart';
 import 'package:todo2/database/repository/auth_repository.dart';
 import 'package:todo2/presentation/pages/auth/sign_in_up/controller/refresh_token_controller.dart';
 import 'package:todo2/services/error_service/error_service.dart';
@@ -13,17 +11,15 @@ class UpdateTokenService {
   );
 
   Future<void> updateToken() async {
-    // try {
-    //   log('*** Token is expired *** ');
-    //   log('refresj token :   ${await _secureStorageService.getUserData(type: StorageDataType.refreshToken)} ');
-
-    //   final authResponse = await refreshTokenController.updateToken();
-    //   await _secureStorageService.saveUserData(
-    //       type: StorageDataType.refreshToken, value: authResponse.refreshToken);
-    //   await _secureStorageService.saveUserData(
-    //       type: StorageDataType.accessToken, value: authResponse.accessToken);
-    // } catch (e) {
-    //   throw Failure(e.toString());
-    // }
+    try {
+      log('*** Token is expired *** ');
+      final authResponse = await refreshTokenController.updateToken();
+      await _secureStorageService.saveUserData(
+          type: StorageDataType.refreshToken, value: authResponse.refreshToken);
+      await _secureStorageService.saveUserData(
+          type: StorageDataType.accessToken, value: authResponse.accessToken);
+    } catch (e) {
+      throw Failure(e.toString());
+    }
   }
 }
