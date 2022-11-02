9d80ee69381bc4a4b709f7efbc3a9e2421f37a0c
peter3374 <peter4533@mail.ru>
2022-11-02 12:16:01 +0300

    unfocus before message

--- lib/services/message_service/message_service.dart
+++ lib/services/message_service/message_service.dart
@@ -6,28 +6,30 @@ class MessageService {
     required BuildContext context,
     required String message,
     int milliseconds = 2500,
-  }) =>
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              padding: const EdgeInsets.only(bottom: 600),
-              elevation: 0,
-              behavior: SnackBarBehavior.floating,
-              backgroundColor: Colors.transparent,
-              content: Container(
-                width: double.infinity,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.black.withOpacity(0.8),
-                ),
-                child: Center(
-                  child: Text(
-                    message,
-                    maxLines: null,
-                  ),
-                ),
-              ),
-              duration: Duration(milliseconds: milliseconds),
+  }) {
+    FocusScope.of(context).unfocus();
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        padding: const EdgeInsets.only(bottom: 600),
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        content: Container(
+          width: double.infinity,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.black.withOpacity(0.8),
+          ),
+          child: Center(
+            child: Text(
+              message,
+              maxLines: null,
             ),
-          );
+          ),
+        ),
+        duration: Duration(milliseconds: milliseconds),
+      ),
+    );
+  }
 }
