99e679037a4966ebbb25990b39fe68c14d9db8e1
storm265 <peter4533@mail.ru>
2023-07-28 22:22:18 +0300

    checklsit item scheme refactor

--- lib/schemas/database_scheme/check_list_items_scheme.dart
+++ lib/schemas/database_scheme/check_list_items_scheme.dart
@@ -1,7 +1,12 @@
 class CheckListItemsScheme {
+
   static const id = 'id';
+
   static const content = 'content';
+
   static const isCompleted = 'is_completed';
+
   static const checklistId = 'checklist_id';
+  
   static const createdAt = 'created_at';
 }
