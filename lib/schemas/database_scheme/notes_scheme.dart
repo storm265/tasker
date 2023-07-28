9a9e8c4b23e9dfe73c7218a2a2d13d77f1c6691f
storm265 <peter4533@mail.ru>
2023-07-28 22:23:06 +0300

    notes scheme refactor

--- lib/schemas/database_scheme/notes_scheme.dart
+++ lib/schemas/database_scheme/notes_scheme.dart
@@ -1,10 +1,17 @@
 class NotesScheme {
   static const data = 'data';
+
   static const message = 'message';
+
   static const id = 'id';
+
   static const description = 'description';
+
   static const color = 'color';
+
   static const isCompleted = 'is_completed';
+
   static const ownerId = 'owner_id';
+
   static const createdAt = 'created_at';
 }
