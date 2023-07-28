e1d460b7ff8227f634842c896e244276e2d3be95
storm265 <peter4533@mail.ru>
2023-07-28 22:24:16 +0300

    scheme refactor

--- lib/schemas/database_scheme/stats_scheme.dart
+++ lib/schemas/database_scheme/stats_scheme.dart
@@ -1,7 +1,11 @@
 class StatsScheme {
   static const createdTasks = 'created_tasks';
+
   static const completedTasks = 'completed_tasks';
+
   static const events = 'events';
+
   static const quickNotes = 'quick_notes';
+
   static const todo = 'todo';
 }
--- lib/schemas/database_scheme/user_data_scheme..dart
+++ lib/schemas/database_scheme/user_data_scheme..dart
@@ -2,12 +2,20 @@
 
 class UserDataScheme {
   static const id = 'id';
+
   static const username = 'username';
+
   static const data = 'data';
+
   static const message = 'message';
+
   static const email = 'email';
+
   static const password = 'password';
+
   static const avatarUrl = 'avatar_url';
+
   static const ownerId = 'owner_id';
+
   static const createdAt = 'created_at';
 }
