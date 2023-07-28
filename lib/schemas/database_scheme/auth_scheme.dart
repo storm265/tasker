cdad95b8d1f680188553c15b685dcbc049ed0e4c
storm265 <peter4533@mail.ru>
2023-07-28 22:21:51 +0300

    refactor auth scheme

--- lib/schemas/database_scheme/auth_scheme.dart
+++ lib/schemas/database_scheme/auth_scheme.dart
@@ -1,15 +1,27 @@
 class AuthScheme {
   static const id = 'id';
+
   static const userId = 'user_id';
+
   static const data = 'data';
+
   static const email = 'email';
+
   static const password = 'password';
+
   static const username = 'username';
+
   static const message = 'message';
+
   static const avatarUrl = 'avatar_url';
+
   static const userSession = 'user_session';
+
   static const accessToken = 'access_token';
+
   static const refreshToken = 'refresh_token';
+
   static const expiresIn = 'expires_in';
+
   static const createdAt = 'created_at';
 }
