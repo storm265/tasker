b65ff8d07609f66dfab6631bfb39ff68697912c9
storm265 <peter4533@mail.ru>
2023-07-28 22:23:30 +0300

    quick notes scheme refactor

--- lib/schemas/database_scheme/quick_note_scheme.dart
+++ lib/schemas/database_scheme/quick_note_scheme.dart
@@ -1,9 +1,15 @@
 class QuickNoteScheme {
   static const checklistId = 'checklistId';
+
   static const title = 'title';
+
   static const color = 'color';
+
   static const content = 'content';
+
   static const isCompleted = 'is_completed';
+
   static const ownerId = 'owner_id';
+
   static const createdAt = 'created_at';
 }
