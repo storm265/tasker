e966aa2687c465053db3e878d1fcbf810e2de292
storm265 <peter4533@mail.ru>
2023-07-28 22:22:44 +0300

    checklist scheme refactor

--- lib/schemas/database_scheme/checklists_scheme.dart
+++ lib/schemas/database_scheme/checklists_scheme.dart
@@ -1,12 +1,17 @@
 class CheckListsScheme {
   static const data = 'data';
-    static const message ='message';
+
+  static const message = 'message';
 
   static const id = 'id';
-  static const title = 'title';
-  static const color = 'color';
-  static const ownerId = 'owner_id';
-  static const items = 'items';
-  static const createdAt = 'created_at';
 
+  static const title = 'title';
+
+  static const color = 'color';
+
+  static const ownerId = 'owner_id';
+
+  static const items = 'items';
+
+  static const createdAt = 'created_at';
 }
