cfafe639c0fc11666b56a32285bd8b10d073039a
storm265 <peter4533@mail.ru>
2023-07-28 22:25:49 +0300

    refactor

--- lib/schemas/projects/project_dao.dart
+++ lib/schemas/projects/project_dao.dart
@@ -17,7 +17,7 @@ abstract class ProjectDao {
   Future<int> insertProject(ProjectTableCompanion entity);
 
   Future<int> deleteProject(String id);
-  
+
   Future<int> deleteAllProjects();
 }
 
@@ -28,38 +28,27 @@ class ProjectDaoImpl extends DatabaseAccessor<ProjectDatabase>
   ProjectDaoImpl(ProjectDatabase projectDatabase) : super(projectDatabase);
 
   @override
-  Future<List<Project>> getProjects() async {
-    return await select(projectTable).get();
-  }
+  Future<List<Project>> getProjects() => select(projectTable).get();
 
   @override
-  Stream<List<Project>> getProjectsStream() {
-    return select(projectTable).watch();
-  }
+  Stream<List<Project>> getProjectsStream() => select(projectTable).watch();
 
   @override
-  Future<Project> getProject(String id) async {
-    return await (select(projectTable)..where((tbl) => tbl.id.equals(id)))
-        .getSingle();
-  }
+  Future<Project> getProject(String id) =>
+      (select(projectTable)..where((tbl) => tbl.id.equals(id))).getSingle();
 
   @override
-  Future<bool> updateProject(ProjectTableCompanion entity) async {
-    return await update(projectTable).replace(entity);
-  }
+  Future<bool> updateProject(ProjectTableCompanion entity) =>
+      update(projectTable).replace(entity);
 
   @override
-  Future<int> insertProject(ProjectTableCompanion entity) async {
-    return await into(projectTable).insert(entity);
-  }
+  Future<int> insertProject(ProjectTableCompanion entity) =>
+      into(projectTable).insert(entity);
 
   @override
-  Future<int> deleteProject(String id) async {
-    return await (delete(projectTable)..where((tbl) => tbl.id.equals(id))).go();
-  }
+  Future<int> deleteProject(String id) =>
+      (delete(projectTable)..where((tbl) => tbl.id.equals(id))).go();
 
   @override
-  Future<int> deleteAllProjects() async {
-    return await (delete(projectTable)).go();
-  }
+  Future<int> deleteAllProjects() => (delete(projectTable)).go();
 }
